ce896b68e6b1f191242d0274601b1be1de1fd22f
Ehsan Karimi <karimiehsan901@gmail.com>
2020-07-14 21:58:13 +0430

    Install stream processor and nlp on production

---
+++ stream-processor/src/main/java/ir/ac/sbu/data_mining/annotation/PrometheusMetric.java
@@ -0,0 +1,12 @@
+package ir.ac.sbu.data_mining.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrometheusMetric {
+    String metricName();
+}
